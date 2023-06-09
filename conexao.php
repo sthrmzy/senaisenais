7529d7b3aba0707d62b9d798454498686476ad70
sthrmzy <119270950+sthrmzy@users.noreply.github.com>
2023-06-08 23:22:53 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,12 +1,12 @@
 <?php
-$host="localhost";
-$user="root";
-$pass="";
-$db ="escola";
 
-$conn = new mysqli($host, $user, $pass, $db);
+function connect(){
+    global $connect;
+    $connect = mysqli_connect(
+        "https:// lisadb-do-user-14164425-0.b.db.ondigitalocean.com",
+        "doadmin",
+        "AVNS_rN3CNWKZ1FkAYE1X6AD",
+        "defaultdb") or die("Erro de conexão com o banco de dados";
 
-if ($conn->connect_error){
-  die('Erro na conexão: ' . $conn->connect_error);
+        )
 }
-?>
